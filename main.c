bfedb7c564eb9d321697c79a0b63d7f3077c1f5f
Alp Berkman <alpberkman@gmail.com>
2023-02-15 04:04:30 +0100

    Tried to write into stdin of terminal but doesnt work correctly

--- main.c
+++ main.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
-#include <string.h>
+#include <unistd.h>
 #include "ff.h"
 
 
 
-int main() {
+int main(int argc, char *argv[]) {
 
     VM vm;
     init(&vm);
     words(&vm);
+    /*
+    char in_path[32];
+    sprintf(in_path, "/proc/%i/fd/0", getpid());
+    FILE *in = fopen(in_path, "w");
+    for(int i = 1; i < argc; ++i) {
+    	char buf[1024];
+    	FILE *src = fopen(argv[i], "r");
+    	
+    	if(src == NULL) {
+    	    printf("Couldn't open %s\n", argv[i]);
+    		continue;
+    	}
+    	
+    	while(fgets(buf, 1023, src) != 0)
+    		fputs(buf, in);
+    	fclose(src);
+    }
+    fclose(in);
+    //return 0;
+    */
     run(&vm);
     debug(&vm);
     dump(&vm, "dump.c");
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
 clear && cat fsrc/op.f fsrc/ex.f fsrc/pg.f - | ./ff
+#./ff fsrc/op.f fsrc/ex.f fsrc/pg.f
